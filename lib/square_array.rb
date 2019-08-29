a83dbf9fee6f39e828154d9f33ec73e91d78a113
Savanna Winland <vgwinland@gmail.com>
2019-08-29 22:47:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ counter = 0
 numbers = [1, 2, 3]
  
 while numbers[counter] do
-  new_numbers = [numbers ** 2]
-  puts new_number[counter]
+  new_numbers = [numbers[counter] ** 2]
+  puts new_number
   counter += 1
 end
 end
