e38177985b8460d427734c92653f26c61b916d0e
Savanna Winland <vgwinland@gmail.com>
2019-08-29 23:01:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
 counter = 0
 numbers = [1, 2, 3]
  
-while counter < numbers.length do
+while counter < numbers.length do |num|
   new_numbers = [numbers * numbers]
   p new_number
   counter += 1
