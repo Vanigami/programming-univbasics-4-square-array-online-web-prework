fdfbddfa6bc270baa66026ead55dba061afe8d69
Savanna Winland <vgwinland@gmail.com>
2019-08-29 22:52:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
 counter = 0
 numbers = [1, 2, 3]
  
-while counter < numbers
+while counter < numbers.length
   new_numbers = [(numbers ** 2)]
   puts new_number
   counter += 1
